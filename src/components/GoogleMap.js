cc454556d17781475823236bf8f9790d2d494f46
Tia Willis <tiawillis1@icloud.com>
2023-01-29 11:53:12 -0700

    Merge branch 'main' of https://github.com/sunnybysunny/TravelTrackerFrontEnd

---
+++ src/components/GoogleMap.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./GoogleMap.css";
+
+function GoogleMap() {
+  return <div className="GoogleMap"></div>;
+}
+
+export default GoogleMap;
