066b472b1f9362fe6798cbff072707bff0770468
Tia Willis <tiawillis1@icloud.com>
2023-01-30 14:39:43 -0700

    broken code hehe

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import { useGoogleLogin } from "@react-oauth/google";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-
 function Home() {
   const navigate = useNavigate();
   const getUserInfo = async (access_token) => {
@@ -27,36 +26,26 @@ function Home() {
           // variables/params for user sub and name, can make these proptypes so they can be used profile?
           params: {
             name: userInfo.given_name,
-            sub: userInfo.sub, 
+            sub: userInfo.sub,
           },
         }
       );
+
+      const Data = {
+        name: userInfo.given_name,
+        sub: userInfo.sub,
+      };
+      navigate("/Profiles/profile_id", { state: Data });
 
       // const toProfile =()=> {navigate('/Profile',{params:{name:'userInfo.given_name',sub:'userInfo.sub'}});}
 
       // const toProfile =()=> {navigate('/Profile',{state:{name:userLoginResult.data}});}
 
-      const item = () => {
-        const nav = useNavigate();
-
-      const Data = {
-        name: userInfo.given_name,
-        sub: userInfo.sub
-      };
-
-      <div onClick={() => {
-        nav("/Profile", { state:Data});
-       }}>Clickme
-       </div>
-       }
-
-
-
       // userLoginResult.data.profile pass this to profile.js in order to render profile info on page
 
       console.log(res.data);
-      console.log(userLoginResult); //pass down 
-      navigate("/profiles/profile_id");
+      console.log(userLoginResult); //pass down
+      // navigate("/profiles/profile_id");
     } catch (err) {
       console.log(err);
     }
@@ -68,12 +57,6 @@ function Home() {
     },
   });
 
-  let content = (
-    <button className="GoogleSignInButton" onClick={googleSignIn}>
-      Sign In With Google
-    </button>
-  );
-
   return (
     <div className="App">
       <header className="App-header"></header>
@@ -82,8 +65,17 @@ function Home() {
       <h6 className="ExplinationHeader">
         WHERE TRAVELERS TRACK THEIR ADVENTURES!
       </h6>
-
-      {content}
+      <button className="GoogleSignInButton" onClick={googleSignIn}>
+        Sign In With Google
+      </button>
+      {/* <div onClick={() => {
+          const Data = {
+            name: userInfo.given_name,
+            sub: userInfo.sub
+          };
+          navigate("/Profile", { state:Data});
+        }}>Clickme
+        </div> */}
     </div>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,13 +2,29 @@ import "./Home.css";
 import axios from "axios";
 import { useGoogleLogin } from "@react-oauth/google";
 import React, { useState } from "react";
-import {useLocation} from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
-const Profile = (props) => {
+const Profile = () => {
+  const location = useLocation();
 
-  const location = useLocation(); 
+  const data = location.state;
+  console.log(data);
 
-  return <h1> HII {location.state}</h1>;
+  return (
+    <h1>
+      HII {data.name} {data.sub}
+    </h1>
+  );
+
+  //   import { useLocation } from 'react-router-dom';
+  // const Profile = () =>{
+  // const location = useLocation();
+  // //the data here will be an object since an object was
+  // const data = location.state;
+  // console.log(data);
+  // <p>data.name</p>
+  // <p>data.phoneNumber</p>
+  // }
   // make sure to put these componenets in order of how you want them to render on the page
   // example below shows how to add them
   // return <GoogleMap />;
