fa36aaaf6b3c0f96ad075a98eb8b903681a55440
Tia Willis <tiawillis1@icloud.com>
2023-01-29 23:40:48 -0700

    implemeneted react router for profile page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
+import React, { useState } from "react";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import AddPin from "./components/AddPin";
 // import GoogleMap from "./components/GoogleMap";
@@ -11,57 +14,13 @@ import jwt_decode from "jwt-decode";
 // import Settings from "/components/Settings";
 
 function App() {
-  const testBackendRequest = async () => {
-    const indexPage = await axios.get("http://localhost:5000");
-    debugger;
-  };
-
-  const getUserInfo = async (access_token) => {
-    try {
-      const res = await axios.get(
-        "https://www.googleapis.com/oauth2/v3/userinfo",
-        {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        }
-      );
-
-      const userInfo = res.data;
-
-      const userLoginResult = await axios.get("http://localhost:5000/login", {
-        params: {
-          name: userInfo.given_name,
-          sub: userInfo.sub,
-        },
-      });
-
-      console.log(res.data);
-      console.log(userLoginResult);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const googleSignIn = useGoogleLogin({
-    onSuccess: async (response) => {
-      getUserInfo(response.access_token);
-    },
-  });
-
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <h3 className="welcomeHeader">WELCOME TO</h3>
-      <h1 className="PushPinMapHeader">PUSH PIN MAP</h1>
-      <h6 className="ExplinationHeader">
-        WHERE TRAVLERS TRACK THERE ADVENTURES!
-      </h6>
-
-      <button className="GoogleSignInButton" onClick={googleSignIn}>
-        Sign In With Google
-      </button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/profiles/profile_id" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,70 @@
+// import logo from "./logo.svg";
+import "./Home.css";
+import axios from "axios";
+import { useGoogleLogin } from "@react-oauth/google";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+  const getUserInfo = async (access_token) => {
+    try {
+      const res = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      const userInfo = res.data;
+
+      const userLoginResult = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/login`,
+        {
+          // variables/params for user sub and name, can make these proptypes so they can be used profile?
+          params: {
+            name: userInfo.given_name,
+            sub: userInfo.sub,
+          },
+        }
+      );
+
+      // userLoginResult.data.profile pass this to profile.js in order to render profile info on page
+
+      console.log(res.data);
+      console.log(userLoginResult);
+      navigate("/profiles/profile_id");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const googleSignIn = useGoogleLogin({
+    onSuccess: async (response) => {
+      getUserInfo(response.access_token);
+    },
+  });
+
+  let content = (
+    <button className="GoogleSignInButton" onClick={googleSignIn}>
+      Sign In With Google
+    </button>
+  );
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h3 className="welcomeHeader">WELCOME TO</h3>
+      <h1 className="PushPinMapHeader">PUSH PIN MAP</h1>
+      <h6 className="ExplinationHeader">
+        WHERE TRAVLERS TRACK THERE ADVENTURES!
+      </h6>
+
+      {content}
+    </div>
+  );
+}
+
+export default Home;
