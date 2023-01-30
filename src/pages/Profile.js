8a42f16bff2538b22a03c317b59a2de344497b2b
Tia Willis <tiawillis1@icloud.com>
2023-01-29 23:40:02 -0700

    Implemented react router for profile page redirect after google login from homepage

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+import axios from "axios";
+import { useGoogleLogin } from "@react-oauth/google";
+import React, { useState } from "react";
+
+const Profile = (props) => {
+  return <h1>Hi this is our profile page using react router</h1>;
+  // make sure to put these componenets in order of how you want them to render on the page
+  // example below shows how to add them
+  // return <GoogleMap />;
+};
+
+export default Profile;
