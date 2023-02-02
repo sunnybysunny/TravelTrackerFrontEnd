5f7d4a71ffffd4c866eec8d5253ee4abbfe3dc47
Tia Willis <tiawillis1@icloud.com>
2023-02-02 10:47:46 -0700

    fixed popups

--- src/components/AddPin.js
+++ src/components/AddPin.js
@@ -1,21 +1,31 @@
 import React from "react";
 // import PropTypes from "prop-types";
 import "./AddPin.css";
-// import { useState } from "react";
+import { useState } from "react";
 
-function AddPin(props){
-  return(props.trigger) ? (
-    <div classname="popup">
+const AddPin = (props) => {
+  const [formFields, setFormFields] = useState({
+    title: "",
+    owner: "",
+  });
+
+  return props.trigger ? (
+    <form className="popup">
       <popup className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          close
+        </button>
         <label>Location:</label>
-          <input type="text" placeholder="City, State/Country"/>
+        <input type="text" placeholder="City, State/Country" />
         <label>Travel Date:</label>
-        <input type="text"placeholder="dd/mm/yyyy"/>
+        <input type="text" placeholder="dd/mm/yyyy" />
+        <input type="submit" value="Submit" />
         {props.children}
       </popup>
-    </div>
-  ): "";
-}
+    </form>
+  ) : (
+    ""
+  );
+};
 
 export default AddPin;
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,21 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Settings.css";
 
-
-function Settings(props){
-  return(props.trigger) ? (
-    <div classname="popup">
+function Settings(props) {
+  return props.trigger ? (
+    <div className="popup">
       <popup className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          close
+        </button>
         <button>Dark Mode</button>
         <button>Light Mode</button>
         <button>Delete Profile</button>
         {props.children}
       </popup>
     </div>
-  ): "";
+  ) : (
+    ""
+  );
 }
 
 export default Settings;
-
-
